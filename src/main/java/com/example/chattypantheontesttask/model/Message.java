f4dd1558600113fe6dd12c771de732b7ed7d9db3
MRiabov <maksymriabov2004@gmail.com>
2022-10-07 16:26:41 +0200

    Message doesn't contain useless information now.

--- src/main/java/com/example/chattypantheontesttask/model/Message.java
+++ src/main/java/com/example/chattypantheontesttask/model/Message.java
@@ -2,19 +2,15 @@ package com.example.chattypantheontesttask.model;
 
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class Message {
     @NonNull
     private final String text;
-    private final User sentTo;
     private final User sentBy;
 
     public Message(@NonNull String text, User sentBy) {
         this.text = text;
         this.sentBy = new User(null, sentBy.getUsername());
-        this.sentTo=null;
     }
 }
