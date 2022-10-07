4e0b409df87d6a3765d0d598b20cd72bce1796ea
MRiabov <maksymriabov2004@gmail.com>
2022-10-07 13:49:28 +0200

    Created new module for users.

---
+++ src/test/java/com/example/chattypantheontesttask/ChattyPantheonTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chattypantheontesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyPantheonTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
