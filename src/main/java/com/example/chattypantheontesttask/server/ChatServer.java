56373d90331f1124810228f550b818cdd6da1a09
MRiabov <maksymriabov2004@gmail.com>
2022-10-07 10:46:47 +0200

    ChatServer is now incapsulated.

--- src/main/java/com/example/chattypantheontesttask/server/ChatServer.java
+++ src/main/java/com/example/chattypantheontesttask/server/ChatServer.java
@@ -1,18 +1,37 @@
 package com.example.chattypantheontesttask.server;
 
 import com.example.chattypantheontesttask.model.User;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
-@Getter
 @Component
 public class ChatServer {
-//    private final int port;
     private final Map<String, User> connectedUsers;
 
+    public boolean quit(String username){
+        return connectedUsers.remove(username) != null;
+    }
 
+    public void register(User user){
+        connectedUsers.put(user.getUsername(),user);
+    }
+
+    public List<String> getAllUsers(){
+        return connectedUsers.keySet().stream().toList();
+    }
+
+    public Optional<User> getByUsername(String username){
+        return Optional.ofNullable(connectedUsers.get(username));
+    }
+
+    public Optional<User> getByHost(String host){
+        return connectedUsers.values().stream()
+                .filter(u -> u.getHost().equals(host))
+                .findAny();
+    }
 }
